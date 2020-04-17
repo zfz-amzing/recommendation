c353b1791994b4125f7225ee83e8521a487c6a76
zfz-amzing <872341267@qq.com>
2020-04-17 10:43:27 +0800

    迁移到笔记本

--- src/main/java/com/zfz/recommendation/controller/BookController.java
+++ src/main/java/com/zfz/recommendation/controller/BookController.java
@@ -1,14 +1,12 @@
 package com.zfz.recommendation.controller;
 
 import com.zfz.recommendation.bean.Book;
-import com.zfz.recommendation.bean.ResponseMessage;
 import com.zfz.recommendation.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
